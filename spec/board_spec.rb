08b282b0a6fb74e30141d3124733fca54b19e9d6
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-04-08 16:21:07 -0600

    Show #render within Board class functional

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -130,5 +130,15 @@ RSpec.describe Board do
       end
     end
 
+    describe 'board rendering' do 
+      it 'shows the board rendering' do
+        board = Board.new
+        cruiser = Ship.new("Cruiser", 3) 
 
+        board.place(cruiser, ["A1", "A2", "A3"])
+        
+        expect(board.render).to eq("  1 2 3 4 \nA . . . . \nB . . . . \nC . . . . \nD . . . . \n")
+        expect(board.render(true)).to eq("  1 2 3 4 \nA S S S . \nB . . . . \nC . . . . \nD . . . . \n")
+      end
+    end
 end
