b8d054b5b2d00d4a508d5867707aa44dc26957bd
Lito Croy <146680987+litobot@users.noreply.github.com>
2024-04-10 14:58:14 -0600

    Refactor and simplify #render logic

--- lib/cell.rb
+++ lib/cell.rb
@@ -32,24 +32,38 @@ class Cell
   end
 
   def render(reveal_ship = false)
-    if !@ship.nil?
-      if @fired_upon
-        if @ship.health > 0
-          "H" # Hit but not sunk
-        else
-          "X" # Sunk
-        end
-      elsif reveal_ship
-        "S" # Ship present but not fired upon, revealed voluntarily
-      else
-        "." # Ship present but not revealed
-      end
-    elsif @fired_upon
-      "M" # Miss
-    else
-      "." # Empty cell or ship hidden
+    if !empty? && fired_upon && @ship.sunk?
+      "X"
+    elsif !empty? && fired_upon 
+      "H"
+    elsif empty? && fired_upon
+      "M"
+    elsif reveal_ship
+      "S"
+    else 
+      "."
     end
   end
+
+  # def render(reveal_ship = false)
+  #   if !@ship.nil?
+  #     if @fired_upon
+  #       if @ship.health > 0
+  #         "H" # Hit but not sunk
+  #       else
+  #         "X" # Sunk
+  #       end
+  #     elsif reveal_ship
+  #       "S" # Ship present but not fired upon, revealed voluntarily
+  #     else
+  #       "." # Ship present but not revealed
+  #     end
+  #   elsif @fired_upon
+  #     "M" # Miss
+  #   else
+  #     "." # Empty cell or ship hidden
+  #   end
+  # end
   
   
 end
